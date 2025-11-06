4d65e212591717d8f7fb5223b63b056139f1ff7c
luiskoche2020 <luiskoche2006@gmail.com>
2025-11-05 21:26:32 -0300

    Meu primeiro commit do projeto

---
+++ src/main/java/br/edu/atitus/api_example/controllers/AuthController.java
@@ -0,0 +1,23 @@
+package br.edu.atitus.api_example.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.atitus.api_example.dtos.SignupDTO;
+import br.edu.atitus.api_example.entities.UserEntity;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+	
+	@PostMapping("/signup")
+	public ResponseEntity<UserEntity> postSignup(@RequestBody SignupDTO dto){
+		UserEntity user = new UserEntity();
+		
+		return ResponseEntity.status(201).body(user);
+	}
+	
+}
---
+++ src/main/java/br/edu/atitus/api_example/dtos/SingninDTO.java
@@ -0,0 +1,5 @@
+package br.edu.atitus.api_example.dtos;
+
+public record SingninDTO(String email, String password) {
+
+}
