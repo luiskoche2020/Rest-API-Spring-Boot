75e05f758f9c03f048281ad031fe2c0cd633c91b
luiskoche2020 <luiskoche2006@gmail.com>
2025-11-11 21:30:46 -0300

    Adição dos seguintes packages: UserService, UserRepository e ConfigSecurity.

--- src/main/java/br/edu/atitus/api_example/controllers/AuthController.java
+++ src/main/java/br/edu/atitus/api_example/controllers/AuthController.java
@@ -1,23 +1,49 @@
 package br.edu.atitus.api_example.controllers;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.atitus.api_example.dtos.SignupDTO;
+import br.edu.atitus.api_example.entities.TypeUser;
 import br.edu.atitus.api_example.entities.UserEntity;
+import br.edu.atitus.api_example.services.UserService;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 	
+	//AuthController DEPENDE de um objeto UserService
+	private final UserService service;
+	
+	public AuthController(UserService service) {
+		super();
+		this.service = service;
+	}
+
 	@PostMapping("/signup")
-	public ResponseEntity<UserEntity> postSignup(@RequestBody SignupDTO dto){
+	public ResponseEntity<UserEntity> postSignup(
+			@RequestBody SignupDTO dto) throws Exception{
 		UserEntity user = new UserEntity();
+		BeanUtils.copyProperties(dto, user);
+		user.setType(TypeUser.Common);
 		
+		service.save(user);
+
 		return ResponseEntity.status(201).body(user);
 	}
 	
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<String> exceptionHandler(Exception e){
+		String message = e.getMessage().replaceAll("\r\n", "");
+		return ResponseEntity.badRequest().body(message);
+	}
+
 }
+
+
+
---
+++ src/main/java/br/edu/atitus/api_example/services/UserService.java
@@ -0,0 +1,50 @@
+package br.edu.atitus.api_example.services;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import br.edu.atitus.api_example.entities.UserEntity;
+import br.edu.atitus.api_example.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	private final UserRepository repository;
+	private final PasswordEncoder encoder;
+	
+	public UserService(UserRepository repository, PasswordEncoder encoder) {
+		super();
+		this.repository = repository;
+		this.encoder = encoder;
+	}
+
+
+
+	public UserEntity save(UserEntity user) throws Exception {
+		if (user == null)
+			throw new Exception("Objeto Nulo");
+		
+		if (user.getName() == null || user.getName().isEmpty())
+			throw new Exception("Nome inválido");
+		user.setName(user.getName().trim());
+		
+		if (user.getEmail() == null || user.getEmail().isEmpty())
+			throw new Exception("E-mail inválido");
+		user.setEmail(user.getEmail().trim());
+		
+		if (user.getPassword() == null 
+				|| user.getPassword().isEmpty()
+				|| user.getPassword().length() < 8)
+			throw new Exception("Password inválido");
+		
+		user.setPassword(encoder.encode(user.getPassword()));
+		
+		if (repository.existsByEmail(user.getEmail()))
+			throw new Exception("Já existe usuário cadastrado com este e-mail");
+		
+		repository.save(user);
+		
+		return user;
+	}
+
+}
